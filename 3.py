1f7019dd678436968370c444c8d800394c3eedda
Zachariah K. Sharma <zachariah.sharma@gmail.com>
2017-11-25 07:50:55 -0500

    made a descripetion of myself

---
+++ 3.py
@@ -0,0 +1,15 @@
+my_name='Zach Sharma'
+my_age=9
+my_hight=48#that was a guess
+my_weight=72
+my_eyes='brown'
+my_teeth='white'
+my_hair='brown'
+print(f"lets talk about {my_name}")
+print(f"He's {my_hight} inches tall mabe")
+print(f"He's {my_weight} pounds heavy")
+print("actully that isn't that heavy")
+print(f"He has {my_eyes} eyes and {my_hair} hair")
+print(f"His teeth are normally {my_teeth}")
+total=my_age+my_hight+my_weight
+print(f"if I add {my_age} , {my_hight} and {my_weight} I will get {total}")
