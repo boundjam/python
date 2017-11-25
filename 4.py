4a2d289eb03cf15e98215ddb8fa4083826497744
Zachariah K. Sharma <zachariah.sharma@gmail.com>
2017-11-25 08:29:18 -0500

    I made a even more dorky thing

---
+++ 4.py
@@ -0,0 +1,15 @@
+types_of_people=10
+x=f"there are {types_of_people} types of people"
+binary='binary'
+do_not="don't"
+y=f"people who know {binary} and people who {do_not}"
+print(x)
+print(y)
+print(f"I said:'{x}'")
+print(f"I also said:'{y}'")
+hilarious=False
+joke_evaloation="isn't that joke so funny?! {}"
+print(joke_evaloation.format(hilarious))
+w=("this the left side of...")
+e=("a string with a right side.")
+print(w+e)
