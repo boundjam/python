bcc44ba6a4b32a900914d11a8d5ef523a7c73546
Zachariah K. Sharma <zachariah.sharma@gmail.com>
2017-11-27 18:21:34 -0500

    printing

---
+++ 9.py
@@ -0,0 +1,10 @@
+days="mon tue wed thu sat sun"
+months="jan\nfeb\nmar\njun\njul\naug"
+print("here are the days:",days)
+print("here are they months:",months)
+print("""
+There something going on
+With the the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
