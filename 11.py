ba5562ee13b2dff151da453b7d99023cac12012a
Zachariah K. Sharma <zachariah.sharma@gmail.com>
2017-12-04 17:56:41 -0500

    making a statement that is true that the person does not have to be on the code

---
+++ 11.py
@@ -0,0 +1,8 @@
+print("how old are you?", end='\n')
+age=input()
+print("how tall are you?",end='\n')
+height=input()
+print("how much do you weight?",end='\n')
+weight=input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy")
