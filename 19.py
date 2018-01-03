e3d584815de37280b099903e219f9ccae63dade1
Zachariah Sharma <zachariah.sharma@gmail.com>
2018-01-03 14:02:51 -0500

    I made some file I changed some files, the regular thing

---
+++ 19.py
@@ -0,0 +1,15 @@
+def cheese_and_crackers(cheesesse_count, boxes_of_crackers):
+    print(f"You have {cheesesse_count} cheeses!")
+    print(f"You have {boxes_of_crackers} boxes_of_crackers")
+    print("Man That is enough for a party!")
+    print("get a blanket.\n")
+print("we can just give the fuction numbers directly!:")
+cheese_and_crackers(20, 30)
+print("OR, we use varibles to our script:")
+amount_of_cheese = 10
+amount_of_crackers= 50
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+print("we can do the math inside too:")
+cheese_and_crackers(10 + 20, 5 + 6)
+print("And we can combine the two, varibles and math:")
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
