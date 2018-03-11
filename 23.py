f8a44facbe17e8a08fa346b7c81230cfd229f8ca
Zachariah Sharma <zachariah.sharma@gmail.com>
2018-03-11 16:33:29 -0400

    Making something new

---
+++ 23.py
@@ -0,0 +1,4 @@
+games=['chess','monopoly','snakes and ladders','clue','mindcraft']
+foods=['pbj','hot dogs','applesauce','carrots','strawberries','oranges']
+favrites=games+foods
+print(favrites)
---
+++ 24.py
@@ -0,0 +1,4 @@
+b='Zachariah'
+r='Krish'
+a='Sharma'
+print(f'hi there,{b} {r} {a}')
