f1ce591c035e900962a07ce067f11a15094b1ead
Zachariah Sharma <zachariah.sharma@gmail.com>
2018-09-14 14:40:02 -0400

    making some thing cool

--- open_file.py
+++ open_file.py
@@ -1,1 +1,5 @@
-open_file.open()
+import webbrowser
+def some_web():
+    send = webbrowser.open('https://wcpss.instructure.com/courses/281447/pages/day-28-converting-fractions-and-decimals-opening-video?module_item_id=1662835')
+
+some_web()
