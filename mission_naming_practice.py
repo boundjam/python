990ce26a48c8eddc0ae54c8f93d13556cc0e7f8b
Zachariah Sharma <zachariah.sharma@gmail.com>
2018-09-16 12:26:54 -0400

    making case changes and removing something not required

--- mission_naming_practice.py
+++ mission_naming_practice.py
@@ -3,33 +3,31 @@ import random
 
 def get_question():
     misson_names = ['SPACE TRAVEL','SOLAR PANEL ARRAY','3D PRINTING','CRATER CROSSING','EXTRACTION','SPACE STATION MODULES','SPACE WALK EMERGENCY','AEROBIC EXERCISE','STRENGTH EXERCISE','FOOD PRODUCTION','ESCAPE VELOCITY','SATELLITE ORBITS','OBSERVITORY','METEROID DEFLECTION','LANDER TOUCH-DOWN']
-    print(misson_names)
     while True:
         M = 'what is the name for'
         mission = random.randint(1,15)
-        print(mission)
         if mission < 10:
-            input_prompt = M + ' M0' + str(mission) + ': '
-            answer = input(input_prompt)
-    #         if answer == misson_names[mission].upper() 
-    #             print('you got it!')
-    #             restart = input('do you want to try again?')
-    #         else:
-    #             restart = input('do you want to try again?').upper()
-    #             if restart == 'no' .upper():
-    #                print('thanks for playing')
-    #                break 
-    #     else :
-    #         print(M , ' M' , str(mission))
-    #         answer = input()
-    #         if answer == m[mission].upper() :
-    #             print('you got it!')
-    #             restart = input('do you want to try again?')
-    #         else:
-    #             restart = input('do you want to try again?').upper()
-    #             if restart == 'no' .upper():
-    #                print('thanks for playing')
-    #                break 
+            print(M ,' M0' , str(mission))
+            answer = input()
+            if answer == misson_names[mission - 1].upper(): 
+                print('you got it!')
+                restart = input('do you want to try again?')
+            else:
+                restart = input(' no,do you want to try again?').upper()
+                if restart == 'NO' .upper():
+                   print('thanks for playing')
+                   break 
+        else :
+            print(M , ' M' , str(mission))
+            answer = input()
+            if answer == misson_names[mission - 1].upper() :
+                print('you got it!')
+                restart = input('do you want to try again?')
+            else:
+                restart = input('no,do you want to try again?').upper()
+                if restart == 'NO' .upper():
+                   print('thanks for playing')
+                   break 
 
 
 if __name__ == "__main__":
