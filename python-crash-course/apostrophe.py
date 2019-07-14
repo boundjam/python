e3db147a406e0cdb8aa050da8380b33cc64cb152
Zachariah Sharma <zach@Zachariahs-MacBook-Air.local>
2019-07-14 11:14:13 -0400

    new files

---
+++ python-crash-course/apostrophe.py
@@ -0,0 +1,2 @@
+message = "One of Python's strengths is it's diverse communtity."
+print(message)
---
+++ python-crash-course/front-back.py
@@ -0,0 +1,5 @@
+def flip(str):
+    if len(str) > 0:
+        list_str = list(str)
+        first = list_str[0]
+        list_str[-1] =
